d4a90732fb332950eafc8009a2bddec4032f4b0f
Shekhar Gulati <shekhargulati84@gmail.com>
2017-09-11 15:16:47 +0530

    Changed implementation to use proxy based approach

--- src/main/java/mathchain/IntMathChain.java
+++ src/main/java/mathchain/IntMathChain.java
@@ -1,34 +1,11 @@
 package mathchain;
 
-import java.util.function.IntUnaryOperator;
+public interface IntMathChain {
+    IntMathChain add(int value);
 
-public class IntMathChain {
+    IntMathChain subtract(int value);
 
-    private final int value;
-    private IntUnaryOperator function;
+    IntMathChain multiply(int value);
 
-    public IntMathChain(int value) {
-        this.function = IntUnaryOperator.identity();
-        this.value = value;
-    }
-
-    public IntMathChain add(int value) {
-        this.function = this.function.andThen(i -> Math.addExact(i, value));
-        return this;
-    }
-
-    public IntMathChain subtract(int value) {
-        this.function = this.function.andThen(i -> Math.subtractExact(i, value));
-        return this;
-    }
-
-    public IntMathChain multiply(int value) {
-        this.function = this.function.andThen(i -> Math.multiplyExact(i, value));
-        return this;
-    }
-
-    public int done() {
-        return this.function.applyAsInt(this.value);
-    }
-
+    int done();
 }
--- src/main/java/mathchain/MathChain.java
+++ src/main/java/mathchain/MathChain.java
@@ -1,9 +1,20 @@
 package mathchain;
 
+import java.lang.reflect.Proxy;
+
 public class MathChain {
 
     public static IntMathChain chain(int value) {
-        return new IntMathChain(value);
+        return create(IntMathChain.class, value);
     }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T create(final Class<T> mathChain, int v) {
+        return (T) Proxy.newProxyInstance(
+                mathChain.getClassLoader(),
+                new Class[]{mathChain},
+                new MathProxyInvocationHandler(v));
+    }
+
 
 }
